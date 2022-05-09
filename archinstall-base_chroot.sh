ac28b3872ebe7737f4f99bbb25b9a7d8b44c754c
Marco Colonna <46967291+70V07@users.noreply.github.com>
2022-05-09 14:17:52 +0200

    Create archinstall-base_chroot.sh

---
+++ archinstall-base_chroot.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "[ work ] install editor and network utility" && pacman -S nano dhcpcd dbus-broker
+echo 
+
+# time zone, hardware clock
+
+echo "[ work ] setup time zone" && ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+echo "[ work ] syncronize hardware time" && hwclock --systohc
+echo "[ show ] system time" && timedatectl show
+echo "[ show ] hardware time" && hwclock --show
+echo 
+
+# locale, keymap
+
+echo "[ work ] locale" && sed -i "/^#$LOCALE/ c$LOCALE" /etc/locale.gen
+echo "[ work ] locale" && locale-gen
+echo "[ work ] locale" && echo LANG=$LANGUAGE > /etc/locale.conf
+echo 
+
+# hostname, password
+
+echo "[ work ] hostname" && echo $HOSTNAME > /etc/hostname
+echo -n "[ work ] ROOT ─ " && passwd root
+echo 
+
+# boot loader
+
+# if UEFI uncomment not-UEFI (and viceversa)
+echo "[ work ] boot" && pacman -S grub os-prober
+#echo "[ work ] boot" && pacman -S grub efibootmgr # UEFI
+echo "[ work ] boot" && grub-install $DRIVE
+#echo "[ work ] boot" && grub-install --efi--directory=/efi # UEFI
+echo "[ work ] boot" && grub-mkconfig -o /boot/grub/grub.cfg
+echo
+
+echo now reboot and bypass or disconnect installation support
+echo
